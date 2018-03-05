fedbeb198a6d80831f45f36cc2b7f15e29585512
Unknown <kaija.cummings@gmail.com>
2018-03-05 15:48:28 -0500

    Added a screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
+	pub
 
 	// Use this for initialization
 	void Start () {
