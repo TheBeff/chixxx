ddd8c2c5c304a80e9b0a87124345d2a460473614
Unknown <kaija.cummings@gmail.com>
2018-03-05 21:13:57 -0500

    Censor Bar Movement
    Made the censor bar move better

--- Assets/Scripts/BarMovement.cs
+++ Assets/Scripts/BarMovement.cs
@@ -10,60 +10,66 @@ public class BarMovement : MonoBehaviour {
 	private Rigidbody2D rb;
 	public float rateOfHorizontalSpeed;
 	public float rateOfVerticalSpeed;
-
+	public float horizontalForce;
+	public float verticalForce;
+	// private Vector2 force;
 
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
-			
+
+
 		
 	}//END START
 	
 
 	void FixedUpdate () {
-		
-		if (Input.anyKeyDown) {
-			if (Input.GetKeyDown (upKey))
+
+
+	
+			if (Input.GetKey (upKey))
 				moveUp ();
 			
-			
-			if (Input.GetKeyDown (downKey))
+			//if (Input.GetKeyDown (downKey))
+			if (Input.GetKey(downKey))
 				moveDown ();
 			
-
-			if (Input.GetKeyDown (leftKey))
+			//if (Input.GetKeyDown (leftKey))
+			if (Input.GetKey(leftKey))
 				moveLeft ();
 			
 			
-			if (Input.GetKeyDown (rightKey))
+			//if (Input.GetKeyDown (rightKey))
+			if (Input.GetKey (rightKey))
 				moveRight ();
-	
-		} 
-		else rb.velocity = new Vector2(0,0);
 
-
+		//else rb.velocity = new Vector2(0,0);
 		}//END FIXED UPDATE
 
 	void moveUp() {
-		rb.velocity = new Vector2 (rb.velocity.x, rateOfVerticalSpeed);
-
+		//rb.velocity = new Vector2 (rb.velocity.x, rateOfVerticalSpeed);
+	//	force = new Vector2 (0, verticalForce);
+		rb.AddForce (Vector2.up * verticalForce);
 		}//END MOVE UP
 
 	void moveDown(){
-		rb.velocity = new Vector2 (rb.velocity.x, ((rateOfVerticalSpeed)*-1));
-
+		//rb.velocity = new Vector2 (rb.velocity.x, ((rateOfVerticalSpeed)*-1));
+		//force = new Vector2 (0, verticalForce);
+		rb.AddForce (Vector2.down * verticalForce);
 
 		}//END MOVE DOWN
 
 	void moveLeft (){
-		rb.velocity = new Vector2 (((rateOfHorizontalSpeed)*-1), rb.velocity.y);
+		//rb.velocity = new Vector2 (((rateOfHorizontalSpeed)*-1), rb.velocity.y);
+		//force = new Vector2 (horizontalForce, 0);
+		rb.AddForce (Vector2.left * horizontalForce);
 	
-
 		}//END MOVE LEFT
 
 	void moveRight(){
-		rb.velocity = new Vector2 (rateOfHorizontalSpeed, rb.velocity.y);
-
+		//rb.velocity = new Vector2 (rateOfHorizontalSpeed, rb.velocity.y);
+		//force = new Vector2 (horizontalForce, 0);
+		rb.AddForce (Vector2.right * horizontalForce);	
 		}//END MOVE RIGHT
 
 }//END BAR MOVEMENT SCRIPT
